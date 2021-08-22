aaa06cbeac7d885bae7cabe3978e67c58b381e0f
rahimshahad <rahim.shahad2@gmail.com>
2021-08-22 07:39:48 -0400

    commit tree 9debb4fe0998e0c6e311562388a549bf3d26eb27

--- demo.js
+++ demo.js
@@ -3,37 +3,37 @@
 
 
 let glyphStates = {
-  "♡": "♥",
-  "♥": "♡"
+    "♡": "♥",
+    "♥": "♡"
 };
 
 let colorStates = {
-  "red" : "",
-  "": "red"
+    "red": "",
+    "": "red"
 };
 
 // STEP 1: This code is what lets JavaScript find the elements that we want to make clickable.
 // Without JavaScript, clicking on these heart shapes does nothing. Uncomment
 // this code and refresh the demo page.
 
-// let articleHearts = document.querySelectorAll(".like-glyph");
+let articleHearts = document.querySelectorAll(".like-glyph");
 
 function likeCallback(e) {
-  let heart = e.target;
-  mimicServerCall()
-    .then(function(serverMessage){
-      // STEP 2: Uncomment the next 3 lines.
-      // We'll use Pillar 1 (DOM Manipulation) to update the screen and
-      // mimic Pillar 3 (Server Communication) to only update the screen if the
-      // sending of information to the server succeeds.
-      alert("You notified the server!");
-      // alert(serverMessage);
-      // heart.innerText = glyphStates[heart.innerText];
-      // heart.style.color = colorStates[heart.style.color];
-    })
-    .catch(function(error) {
-      alert("Something went wrong!");
-    });
+    let heart = e.target;
+    mimicServerCall()
+        .then(function(serverMessage) {
+            // STEP 2: Uncomment the next 3 lines.
+            // We'll use Pillar 1 (DOM Manipulation) to update the screen and
+            // mimic Pillar 3 (Server Communication) to only update the screen if the
+            // sending of information to the server succeeds.
+            alert("You notified the server!");
+            alert(serverMessage);
+            heart.innerText = glyphStates[heart.innerText];
+            heart.style.color = colorStates[heart.style.color];
+        })
+        .catch(function(error) {
+            alert("Something went wrong!");
+        });
 }
 
 // STEP 3: In order for the call to the server and the update of the screen to
@@ -41,9 +41,9 @@ function likeCallback(e) {
 // code when an "event" is fired. That's Pillar 2, event handling. Uncomment
 // this code.
 
-//for (let glyph of articleHearts) {
-  //glyph.addEventListener("click", likeCallback);
-//}
+for (let glyph of articleHearts) {
+    glyph.addEventListener("click", likeCallback);
+}
 
 // STEP 4: 
 
@@ -57,9 +57,9 @@ function likeCallback(e) {
 //------------------------------------------------------------------------------
 
 function mimicServerCall() {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      resolve("Pretend remote server notified of action!");
-    }, 300);
-  });
+    return new Promise(function(resolve, reject) {
+        setTimeout(function() {
+            resolve("Pretend remote server notified of action!");
+        }, 300);
+    });
 }
